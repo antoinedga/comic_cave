5dfb81f6b4c8836c75adbf7d5e9b2ca3dcd9a7ca
root <root@batcomputer.localdomain>
2019-11-25 17:39:43 -0500

    working except for image

--- site_admin/add_comic.php
+++ site_admin/add_comic.php
@@ -6,10 +6,11 @@
 	<title>Add a Comic</title>
 </head>
 <body>
-<?php # allows the administrator to add a print (product).
+<?php # allows the administrator to add a comic (product).
 
-require ('../../mysqli_connect.php');
-
+require ('../mysqli_connect.php');
+$conn = OpenCon();
+echo "Connected Successfully";
 if ($_SERVER['REQUEST_METHOD'] == 'POST') 
 { // Handle the form.
 	
@@ -59,7 +60,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 	} 
 	else 
 	{
-		$errors[] = 'Please enter the print\'s price!';
+		$errors[] = 'Please enter the comic\'s price!';
 	}
 
 	// Check for a description (not required):
@@ -76,7 +77,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 	}
 	
 	// Validate the writer...
-	if ( isset($_POST['writer']) && filter_var($_POST['writer'], FILTER_VALIDATE_INT, array('min_range' => 1))) 
+	if ( isset($_POST['writer']) && filter_var($_POST['writer'], FILTER_VALIDATE_INT, array('min_range' => 1))  ) 
 	{
 		$w = $_POST['writer'];
 	} 
@@ -88,16 +89,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 	// Validate the publisher...
 	if ( isset($_POST['publisher']) && filter_var($_POST['publisher'], FILTER_VALIDATE_INT, array('min_range' => 1))  ) 
 	{
-		$p = $_POST['publisher'];
+		$pub = $_POST['publisher'];
 	} 
 	else  // No publisher selected. 
 	{
 		$errors[] = 'Please select the comics\'s publisher!';
 	}
 
-	if ( isset($_POST['quantity']) && filter_var($_POST['quantity'], FILTER_VALIDATE_INT, array('min_range' => 1))  ) 
+	if ( isset($_POST['quan']) && filter_var($_POST['quan'], FILTER_VALIDATE_INT, array('min_range' => 1))  ) 
 	{
-		$quan = $_POST['quantity'];
+		$quan = $_POST['quan'];
 	} 
 	else // No quantity entered. 
 	{ 
@@ -109,8 +110,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 	{ 
 		// Add the comic to the database:
 		$q = 'INSERT INTO comics (artist_id, writer_id, publisher_id, comic_name, price, description, image_name, quantity) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-		$stmt = mysqli_prepare($dbc, $q);
-		mysqli_stmt_bind_param($stmt, 'isdsss', $a, $w, $p, $cn, $p, $d, $i, $quan);
+		$stmt = mysqli_prepare($conn, $q);
+		mysqli_stmt_bind_param($stmt, 'iiisdssi', $a, $w, $pub, $cn, $p, $d, $i, $quan);
 		mysqli_stmt_execute($stmt);
 		
 		// Check the results...
@@ -120,7 +121,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 			echo '<p>The comic has been added.</p>';
 	
 			// Rename the image:
-			$id = mysqli_stmt_insert_id($stmt); // Get the print ID.
+			$id = mysqli_stmt_insert_id($stmt); // Get the comic ID.
 			rename ($temp, "../../uploads/$id");
 	
 			// Clear $_POST:
@@ -150,13 +151,13 @@ if ( !empty($errors) && is_array($errors) )
 	{
 		echo " - $msg<br />\n";
 	}
-	echo 'Please reselect the print image and try again.</p>';
+	echo 'Please reselect the comic image and try again.</p>';
 }
 
 // Display the form...
 ?>
 <h1>Add a Comic</h1>
-<form enctype="multipart/form-data" action="add_print.php" method="post">
+<form enctype="multipart/form-data" action="add_comic.php" method="post">
 
 	<input type="hidden" name="MAX_FILE_SIZE" value="524288" />
 	
@@ -170,7 +171,7 @@ if ( !empty($errors) && is_array($errors) )
 	<select name="artist"><option>Select One</option>
 	<?php // Retrieve all the artists and add to the pull-down menu.
 	$q = "SELECT artist_id, CONCAT_WS(' ', first_name, last_name) FROM artists ORDER BY last_name, first_name ASC";		
-	$r = mysqli_query ($dbc, $q);
+	$r = mysqli_query ($conn, $q);
 	if (mysqli_num_rows($r) > 0) {
 		while ($row = mysqli_fetch_array ($r, MYSQLI_NUM)) {
 			echo "<option value=\"$row[0]\"";
@@ -181,7 +182,7 @@ if ( !empty($errors) && is_array($errors) )
 	} else {
 		echo '<option>Please add a new artist first.</option>';
 	}
-	mysqli_close($dbc); // Close the database connection.
+	// mysqli_close($conn); // Close the database connection.
 	?>
 	</select></p>
 
@@ -189,7 +190,7 @@ if ( !empty($errors) && is_array($errors) )
 	<select name="writer"><option>Select One</option>
 	<?php // Retrieve all the artists and add to the pull-down menu.
 	$q = "SELECT writer_id, CONCAT_WS(' ', first_name, last_name) FROM writers ORDER BY last_name, first_name ASC";		
-	$r = mysqli_query ($dbc, $q);
+	$r = mysqli_query ($conn, $q);
 	if (mysqli_num_rows($r) > 0) {
 		while ($row = mysqli_fetch_array ($r, MYSQLI_NUM)) {
 			echo "<option value=\"$row[0]\"";
@@ -200,15 +201,15 @@ if ( !empty($errors) && is_array($errors) )
 	} else {
 		echo '<option>Please add a new writer first.</option>';
 	}
-	mysqli_close($dbc); // Close the database connection.
+	// mysqli_close($conn); // Close the database connection.
 	?>
 	</select></p>
 
 	<p><b>Publisher:</b> 
 	<select name="publisher"><option>Select One</option>
 	<?php // Retrieve all the artists and add to the pull-down menu.
-	$q = "SELECT publisher_id, CONCAT_WS(' ', publisher) FROM publishers ORDER BY publisher ASC";		
-	$r = mysqli_query ($dbc, $q);
+	$q = "SELECT publisher_id, CONCAT_WS(' ', publisher_name) FROM publishers ORDER BY publisher_name ASC";		
+	$r = mysqli_query ($conn, $q);
 	if (mysqli_num_rows($r) > 0) {
 		while ($row = mysqli_fetch_array ($r, MYSQLI_NUM)) {
 			echo "<option value=\"$row[0]\"";
@@ -219,13 +220,13 @@ if ( !empty($errors) && is_array($errors) )
 	} else {
 		echo '<option>Please add a new publisher first.</option>';
 	}
-	mysqli_close($dbc); // Close the database connection.
+	mysqli_close($conn); // Close the database connection.
 	?>
 	</select></p>
 	
 	<p><b>Price:</b> <input type="text" name="price" size="10" maxlength="10" value="<?php if (isset($_POST['price'])) echo $_POST['price']; ?>" /> <small>Do not include the dollar sign or commas.</small></p>
 	
-	<p><b>Quantity:</b> <input type="text" name="size" size="30" maxlength="60" value="<?php if (isset($_POST['size'])) echo htmlspecialchars($_POST['size']); ?>" /> Quantity in stock</p>
+	<p><b>Quantity:</b> <input type="text" name="quan" size="30" maxlength="10" value="<?php if (isset($_POST['quan'])) echo $_POST['quan']; ?>" /> Quantity in stock</p>
 	
 	<p><b>Description:</b> <textarea name="description" cols="40" rows="5"><?php if (isset($_POST['description'])) echo $_POST['description']; ?></textarea> (optional)</p>
 	
