c0bdefae12af6d4672309682f2b4a189be3213f1
rareg707 <r.regan@knights.ucf.edu>
2019-12-02 02:17:17 -0500

    working

--- site_admin/login.php
+++ site_admin/login.php
@@ -6,12 +6,9 @@ $error = false;
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	require ('../mysqli_connect.php');
 	$conn = OpenCon();
-	$trimmed = array_map('trim', $_POST);
-
 	// Validate the email address:
 	if (!empty($_POST['email'])) {
 		$e = mysqli_real_escape_string($conn, $_POST['email']);
-		echo $e;
 	} else {
 		$e = FALSE;
 		echo '<p class="error">You forgot to enter your email address!</p>';
@@ -19,28 +16,39 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	// Validate the password:
 	if (!empty($_POST['pass'])) {
 		$p = mysqli_real_escape_string ($conn, $_POST['pass']);
-		echo (password_hash($trimmed['pass'], PASSWORD_DEFAULT));
 	} else {
 		$p = FALSE;
 		echo '<p class="error">You forgot to enter your password!</p>';
 	}
 	if ($e && $p) { // If everything's OK.
 		// Query the database:
-		$q = "SELECT user_id, first_name FROM users WHERE (email='$e' AND pass='$p')";
+		echo 'ok';
+		$q = "SELECT * FROM users WHERE email='$e'";
 		$r = mysqli_query($conn, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($conn));
-		
+
 		if (@mysqli_num_rows($r) == 1) { // A match was made.
+			echo 'result';
 			// Fetch the values:
-			$_SESSION = mysqli_fetch_array ($r, MYSQLI_ASSOC); 
+			list($user_id, $first_name, $email, $pass) = mysqli_fetch_array($r, MYSQLI_NUM);
 			mysqli_free_result($r);
-			mysqli_close($conn);
+			// Check the password:
 
-			// Redirect the user:
-			$url = 'dashboard.php'; // Define the URL.
-			ob_end_clean(); // Delete the buffer.
-			header("Location: $url");
-			exit(); // Quit the script.
+			if (password_verify($p, $pass)) {
+				echo 'verified';
 
+				// Store the info in the session:
+				$_SESSION['user_id'] = $user_id;
+				$_SESSION['first_name'] = $first_name;
+				$loggedin=true;
+				mysqli_close($conn);
+				// Redirect the user:
+				$url = 'dashboard.php'; // Define the URL.
+				ob_end_clean(); // Delete the buffer.
+				header("Location: $url");
+				exit(); // Quit the script.
+			} else {
+				echo '<p class="error">Either the email address or password entered do not match those on file.</p>';
+			}
 		} else { // No match was made.
 			echo '<p class="error"> Either the email address or password entered do not match those on file.</p>';
 			printf("outer erro,, no rows T-T %s\n", $e);
@@ -56,8 +64,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <p>Your browser must allow cookies in order to log in.</p>
 <form action="login.php" method="post">
 	<fieldset>
-	<p><strong>Email Address:</strong> <input type="text" name="email" size="20" maxlength="60"></p>
-	<p><strong>Password:</strong> <input type="password" name="pass" size="20"maxlength="20"/></p>
+	<p><strong>Email Address:</strong> <input type="email" name="email" size="20" maxlength="60"></p>
+	<p><strong>Password:</strong> <input type="password" name="pass" size="20" maxlength="20"/></p>
 	<div align="center"><input type="submit" name="submit" value="Login"></div>
 	</fieldset>
 </form>
