7ac92dba19c27e6899b5ac9e4c328dd6ce1eec2d
Antoine <antoinedga@Antoines-MacBook-Pro.local>
2019-12-02 18:51:40 -0500

    fixed sql query

--- site_admin/view_single_comic.php
+++ site_admin/view_single_comic.php
@@ -8,17 +8,18 @@ $row = FALSE; // Assume nothing!
 	$conn = OpenCon();
 	// echo "Connected Successfully";
 	// $conn = OpenCon();
-	$q =  "SELECT
-      CONCAT_WS(' ', artists.first_name, artists.last_name) AS artist, CONCAT_WS(' ', writers.first_name, writers.last_name) AS writer,
-  		 publishers.publisher_name AS publisher,
-      comic_name, quantity, description
-      FROM artists, writers, publishers, comics WHERE
-	-- artists.artist_id=comics.artist_id AND
-	-- writers.writer_id=comics.writer_id AND
-	-- publishers.publisher_id=comics.publisher_id AND
+	$q = "SELECT
+	CONCAT_WS(' ', artists.first_name, artists.last_name) AS artist, CONCAT_WS(' ', writers.first_name, writers.last_name) AS writer, publishers.publisher_name AS publisher,
+	comic_name, price, description, cover_image, quantity
+	FROM artists, writers, publishers, comics
+	WHERE
+	artists.artist_id=comics.artist_id AND
+	writers.writer_id=comics.writer_id AND
+	publishers.publisher_id=comics.publisher_id AND
 	comics.comic_id=$cid";
+	
 	$r = mysqli_query ($conn, $q);
-  $row = mysqli_fetch_object($r);
+  $row = mysqli_fetch_array($r);
   echo json_encode($r);
   mysqli_close($conn);
 
