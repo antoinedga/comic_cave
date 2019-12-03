3ba8f88ff01b978e6da8b307d1ddc70a68640f77
Antoine <antoinedga@Antoines-MacBook-Pro.local>
2019-12-03 01:16:52 -0500

    added back _serverROot

--- site_admin/add_comic.php
+++ site_admin/add_comic.php
@@ -22,7 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 	if (is_uploaded_file ($_FILES['image']['tmp_name']))
 	{
 		// Create a temporary file name:
-		$temp = '../uploads/' . md5($_FILES['image']['name']);
+		$temp = $_SERVER['DOCUMENT_ROOT']."/uploads/";
 
 		// Move the file over:
 		if (move_uploaded_file($_FILES['image']['tmp_name'], $temp))
