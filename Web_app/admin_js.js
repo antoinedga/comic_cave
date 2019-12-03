9249c7e88de798a5c873625350cbb4737ccded53
Antoine <antoinedga@Antoines-MacBook-Pro.local>
2019-12-03 00:44:41 -0500

    attempt 1001 with now image not required

--- Web_app/admin_js.js
+++ Web_app/admin_js.js
@@ -271,7 +271,7 @@ var opt = document.createElement('option');
       form.elements["price"].value = Number(form.elements["price"].value.replace(/[^0-9.-]+/g,""));
       console.log(form);
       var formData = new FormData(form);
-      formData.append('image', img);
+      //formData.append('image', img);
       console.log(img);
       console.log(formData);
 
