707c0a2589f49f4ee0dc71fdde8987a2e7b49ba3
Antoine <antoinedga@Antoines-MacBook-Pro.local>
2019-12-03 01:09:09 -0500

    changing line 25 of add_comic.php to see if error consist

--- site_admin/add_comic.php
+++ site_admin/add_comic.php
@@ -22,7 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 	if (is_uploaded_file ($_FILES['image']['tmp_name']))
 	{
 		// Create a temporary file name:
-		$temp = '../uploads/';
+		$temp = $_SERVER['DOCUMENT_ROOT']."/uploads/";
 
 		// Move the file over:
 		if (move_uploaded_file($_FILES['image']['tmp_name'], $temp))
