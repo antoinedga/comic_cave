0efdca2f9bec4157fe31e8f5d54be4ab344911ef
Antoine <antoinedga@Antoines-MacBook-Pro.local>
2019-12-02 23:26:34 -0500

    attempt 100 with now image not required

--- site_admin/dashboard.php
+++ site_admin/dashboard.php
@@ -4,28 +4,14 @@ session_start();
 $page_title = 'Welcome!';
 // include('includes/header.html');
 // Welcome the user (by name if they are logged in):
-echo '<h1>Welcome';
-if (isset($_SESSION['first_name'])) {
-	echo ", {$_SESSION['first_name']}";
-}
 
 if (!isset($_SESSION['first_name'])) {
 
-	$url = '../index.php'; 
-	ob_end_clean(); 
+	$url = '../index.php';
+	ob_end_clean();
 	header("Location: $url");
-	exit(); 
+	exit();
 }
 
-echo '!</h1>';
 ?>
-<p>Spam spam spam spam spam spam
-spam spam spam spam spam spam
-spam spam spam spam spam spam
-spam spam spam spam spam spam.</p>
-<p>Spam spam spam spam spam spam
-spam spam spam spam spam spam
-spam spam spam spam spam spam
-spam spam spam spam spam spam.</p>
-
-<!-- <?php include('includes/footer.html'); ?> -->
+<!-- add dashboard -->
