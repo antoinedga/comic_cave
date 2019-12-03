7fdb18ae9cf83a1723756c880a192a7ea331f03a
Antoine <antoinedga@Antoines-MacBook-Pro.local>
2019-12-02 23:25:43 -0500

    attempt 100 with now image not required

--- Web_app/admin_js.js
+++ Web_app/admin_js.js
@@ -265,7 +265,7 @@ var opt = document.createElement('option');
 // api call to add comics
     function submitForm_comic(){
       var img = $('input[name="image"]').get(0).files[0];
-      var form = $('#comicForm')[0];
+      var form = $('#comicForm')[0].serialize();
       var formData = new FormData(form);
       formData.append('image', img);
       console.log(form);
