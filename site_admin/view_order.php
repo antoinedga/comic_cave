7cb8100fa2ff5474fd50e09deee9b28333cd9455
Jennifer Lemons <lemons@knights.ucf.edu>
2019-12-04 14:49:48 -0500

    Update view_order.php

--- site_admin/view_order.php
+++ site_admin/view_order.php
@@ -1,8 +1,11 @@
 <?php
+$order = array();
+$result = array();
 require ('../mysqli_connect.php'); // Connect to the database.
 $conn = OpenCon();
 $q = "SELECT 
-	JSON_ARRAYAGG(JSON_OBJECT('comic',comics.comic_name, 'quantity', order_info.quantity )) as items,
+	comics.comic_name as comic,
+	order_info.quantity as quantity,
 	orders.customer_email, 
 	orders.total,
 	orders.order_id,
@@ -15,16 +18,26 @@ AND comics.comic_id=order_info.comic_id";
 
 // Display all the prints, linked to URLs:
 $r = mysqli_query ($conn, $q);
-if (mysqli_num_rows($r) <= 0)
+if ($r)
 {
-	printf("no rows??\n");
+	if (mysqli_num_rows($r) <= 0)
+	{
+		printf("no rows??\n");
+	}
+
+	while ($row = mysql_fetch_array($result))
+	{
+		$order = array("comic_name" => $row['comic'], "quantity" => $row['quantity']);
+		$result[$row['order_id']] = array("customer_email" => $row['order'], "total" => $row['total'], 
+		"order_date" => $row['order_date']);
+		$result[$row['order_id']]['item'][] = $order;
+	}
+	echo json_encode($result);
 }
-$json = mysqli_fetch_all ($r, MYSQLI_ASSOC);
-// while ($row = $json->fetch_assoc())
-// {
-//	;
-//}
-echo json_encode($json);
+else
+{
+	echo mysql_error();
+}
 // order in array
 mysqli_close($conn);
 ?>
