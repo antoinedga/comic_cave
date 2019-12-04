d967fef4b9b0a89b0f8a19e7c2af0f58537a23e4
Jennifer Lemons <lemons@knights.ucf.edu>
2019-12-04 17:34:21 -0500

    Update admin_js.js

--- Web_app/admin_js.js
+++ Web_app/admin_js.js
@@ -118,13 +118,13 @@ var opt = document.createElement('option');
      cache:false,
      success: function(data){
        console.log("before json " +  data);
-       var tem1 = JSON.parse(data)
-       console.log("parse data" + tem1);
-       var str = tem1[0].items;
-       str.replace('"','');
-       var temp = JSON.parse(str);
-       console.log(temp);
-       data.forEach(table_row_order);
+       //var tem1 = JSON.parse(data)
+       //console.log("parse data" + tem1);
+       //var str = tem1[0].items;
+       //str.replace('"','');
+       //var temp = JSON.parse(str);
+       //console.log(temp);
+       //data.forEach(table_row_order);
 
        console.log(data);
        //data.forEach(table_row_order);
