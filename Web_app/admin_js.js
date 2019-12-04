5a95215508daaff49a35eb2f99266cbfcaab36aa
Antoine <antoinedga@Antoines-MacBook-Pro.local>
2019-12-03 23:03:31 -0500

    attempting to fix the form

--- Web_app/admin_js.js
+++ Web_app/admin_js.js
@@ -287,10 +287,9 @@ var opt = document.createElement('option');
 // api call to add comics
     function submitForm_comic(){
       var img = $('input[name="image"]').get(0).files[0];
-      var form = $('form#comicForm');
+      var form = $('#comicForm');
       var formData = new FormData(form);
-      //formData.append('image', img);
-    //  console.log(img);
+      formData.append('image', img);
       console.log(formData);
 
     	 $.ajax({
