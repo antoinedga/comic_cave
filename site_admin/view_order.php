a752e0d60954d831caa5326cd30d07bb27fac44c
Jennifer Lemons <lemons@knights.ucf.edu>
2019-12-04 13:21:05 -0500

    Update view_order.php

--- site_admin/view_order.php
+++ site_admin/view_order.php
@@ -1,9 +1,13 @@
 <?php
 require ('../mysqli_connect.php'); // Connect to the database.
 $conn = OpenCon();
-$q = "SELECT JSON_ARRAYAGG(JSON_OBJECT('comic',comics.comic_name, 'quantity', order_info.quantity )) as items
-,orders.customer_email, orders.total,orders.order_id,
-orders.order_date FROM order_info, orders, comics
+$q = "SELECT 
+	JSON_ARRAYAGG(JSON_OBJECT('comic',comics.comic_name, 'quantity', order_info.quantity )) as items,
+	orders.customer_email, 
+	orders.total,
+	orders.order_id,
+	orders.order_date 
+FROM order_info, orders, comics
 WHERE orders.order_id=order_info.order_id
 AND comics.comic_id=order_info.comic_id";
 
