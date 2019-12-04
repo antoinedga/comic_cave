0488c7285225369fd677c5c65bd31f6837e1c282
Jennifer Lemons <lemons@knights.ucf.edu>
2019-12-04 17:24:25 -0500

    Update view_order.php

--- site_admin/view_order.php
+++ site_admin/view_order.php
@@ -11,6 +11,14 @@ FROM order_info, orders, comics
 WHERE orders.order_id=order_info.order_id
 AND comics.comic_id=order_info.comic_id";
 
+function myfunction($value, $key)
+{
+	if ($key == 'items')
+	{
+		$temp = json_decode($value);
+		$value = clone $temp;
+	}
+}
 // Display all the prints, linked to URLs:
 $r = mysqli_query ($conn, $q);
 if (mysqli_num_rows($r) <= 0)
@@ -18,10 +26,10 @@ if (mysqli_num_rows($r) <= 0)
     printf("no rows??\n");
 }
 $json = mysqli_fetch_all ($r, MYSQLI_ASSOC);
-$temp = json_decode($json[0]['items'], true);
 
+array_walk_recursive($json, "myfunction");
 
-echo json_encode($temp);
+echo json_encode($json);
 // order in array
 mysqli_close($conn);
 ?>
