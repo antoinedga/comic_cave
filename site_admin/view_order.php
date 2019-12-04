3dcea43fe2f2c8b5b6de092198425e41c9817a0d
Jennifer Lemons <lemons@knights.ucf.edu>
2019-12-04 17:00:11 -0500

    Update view_order.php

--- site_admin/view_order.php
+++ site_admin/view_order.php
@@ -10,15 +10,28 @@ $q = "SELECT
 FROM order_info, orders, comics
 WHERE orders.order_id=order_info.order_id
 AND comics.comic_id=order_info.comic_id";
-// Create the table head:
+
 // Display all the prints, linked to URLs:
 $r = mysqli_query ($conn, $q);
 if (mysqli_num_rows($r) <= 0)
 {
     printf("no rows??\n");
 }
-echo $json;
+$temp = array();
 $json = mysqli_fetch_all ($r, MYSQLI_ASSOC);
+
+$constants = get_defined_constants(true);
+$json_errors = array();
+foreach ($constants["json"] as $name => $value) {
+    if (!strncmp($name, "JSON_ERROR_", 11)) {
+        $json_errors[$value] = $name;
+    }
+}
+
+// Show the errors for different depths.
+var_dump(json_decode($json, true, 4));
+echo 'Last error: ', $json_errors[json_last_error()], PHP_EOL, PHP_EOL;
+
 echo json_encode($json);
 // order in array
 mysqli_close($conn);
