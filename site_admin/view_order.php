7a393df8bcde0678cfe11546c6393c8ee95701fb
Antoine <antoinedga@Antoines-MacBook-Pro.local>
2019-12-04 18:23:25 -0500

    lemons got the group by working

--- site_admin/view_order.php
+++ site_admin/view_order.php
@@ -1,15 +1,15 @@
 <?php
 require ('../mysqli_connect.php'); // Connect to the database.
 $conn = OpenCon();
-$q = "SELECT 
+$q = "SELECT
     JSON_ARRAYAGG(JSON_OBJECT('comic',comics.comic_name, 'quantity', order_info.quantity )) as items,
-    orders.customer_email, 
+    orders.customer_email,
     orders.total,
     orders.order_id,
-    orders.order_date 
+    orders.order_date
 FROM order_info, orders, comics
 WHERE orders.order_id=order_info.order_id
-AND comics.comic_id=order_info.comic_id";
+AND comics.comic_id=order_info.comic_id GROUP BY orders.order_id";
 
 // Display all the prints, linked to URLs:
 $r = mysqli_query ($conn, $q);
