45479d877268c4618397cd79e93cc76adc4bd79b
Antoine <antoinedga@Antoines-MacBook-Pro.local>
2019-12-04 14:12:51 -0500

    why god

--- site_admin/view_order.php
+++ site_admin/view_order.php
@@ -1,12 +1,12 @@
 <?php
 require ('../mysqli_connect.php'); // Connect to the database.
 $conn = OpenCon();
-$q = "SELECT 
-	JSON_ARRAYAGG(JSON_OBJECT('comic',comics.comic_name, 'quantity', order_info.quantity )) as items,
-	orders.customer_email, 
+$q = "SELECT JSON_ARRAYAGG(
+	JSON_OBJECT('comic',comics.comic_name, 'quantity', order_info.quantity )) as items,
+	orders.customer_email,
 	orders.total,
 	orders.order_id,
-	orders.order_date 
+	orders.order_date
 FROM order_info, orders, comics
 WHERE orders.order_id=order_info.order_id
 AND comics.comic_id=order_info.comic_id";
@@ -19,7 +19,6 @@ if (mysqli_num_rows($r) <= 0)
 {
 	printf("no rows??\n");
 }
-echo $json;
 $json = mysqli_fetch_all ($r, MYSQLI_ASSOC);
 echo json_encode($json);
 // order in array
